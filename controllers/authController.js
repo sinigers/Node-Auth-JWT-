7c4abe5d6ef9d79f772a4c4653f35c2fe5cc20e9
sinigers <sinigers@yahoo.com>
2021-06-23 11:44:12 +0300

    model user collection

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,19 +1,27 @@
+const User = require('../modules/User');
+
 module.exports.signup_get = (req, res) => {
     res.render('signup');
 }
 
-module.exports.signup_post = (req, res) => {
+module.exports.signup_post = async (req, res) => {
     const { email, password } = req.body;
-    console.log(email, password);
-
-    res.send('new signup');
+    
+    try{
+        const user = await User.create({email, password});
+        res.status(201).json(user);
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send('error, usern not created');
+    }
 }
 
 module.exports.login_get = (req, res) => {
     res.render('login');
 }
 
-module.exports.login_post = (req, res) => {
+module.exports.login_post = async (req, res) => {
     const { email, password } = req.body;
     console.log(email, password);
 

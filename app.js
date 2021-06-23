17f5f899f41b5dd313c136c47e1c77c6bdeb6bf2
sinigers <sinigers@yahoo.com>
2021-06-23 09:09:45 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const authRoutes = require('./routes/authRoutes');
+
+
+const app = express();
+
+// middleware
+app.use(express.static('public'));
+
+// view engine
+app.set('view engine', 'ejs');
+
+// database connection
+const dbURI = 'mongodb+srv://user:passs@cluster0.cqyh9.mongodb.net/node-auth?retryWrites=true&w=majority';
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get('/', (req, res) => res.render('home'));
+app.get('/smoothies', (req, res) => res.render('smoothies'));
+app.use(authRoutes);

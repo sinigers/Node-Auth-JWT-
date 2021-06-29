54d779ef348b53a38e4fb1dcafca48e078afbe6f
sinigers <sinigers@yahoo.com>
2021-06-29 17:31:32 +0300

    install cookieParser

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
+const cookieParser = require('cookieParser');
 
 
 const app = express();
@@ -10,6 +11,10 @@ app.use(express.static('public'));
 
 //takes json on post req and parse itinto js object and attached it to req
 app.use(express.json());
+
+app.use(cookieParser());
+
+
 
 // view engine
 app.set('view engine', 'ejs');
@@ -24,3 +29,27 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+//cookies
+app.get('/set-cookies', (req, res) => {
+// //set cookie without cookieParseer
+//   res.setHeader('Set-Cookie', 'newUser=true');
+
+res.cookie('newUser', false);
+
+// one day =  1000 * 60 * 60 * 24, after that will remove/expire cookie, secure - set cookie only on https, httpOnly- cant acces true jS front end ( in console - document.cookie)
+// res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, secure: true});
+res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true});
+res.send('you got the cookies!');
+
+
+
+});
+
+app.get('/read-cookies', (req, res) => {
+const cookies = req.cookies;
+console.log(cookies.newUser);
+
+
+res.json(cookies);
+});

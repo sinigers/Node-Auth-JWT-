9b736808d7feae5736e49e7cda9e3bab666dd05c
sinigers <sinigers@yahoo.com>
2021-06-30 14:15:28 +0300

    labs

--- models/User.js
+++ models/User.js
@@ -31,6 +31,20 @@ userSchema.pre('save', async function(next){
   next();
 });
 
+//static method to lofin user
+userSchema.statics.login = async function(email, password){
+  const user = await this.findOne({ email });
+  if(user){
+    const auth = await bcrypt.compare(password, user.password);
+    if (auth) {
+      return user
+    }
+    throw Error('incorect password');
+  }
+  throw Error('incorect email');
+}
+
+
 //connect to MongoDb collection 
 const User = mongoose.model('user', userSchema);
 
